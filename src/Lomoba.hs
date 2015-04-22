7d96bf5a61773819f0b6437e507288645c45574f
aszyrej <luzbelito.as@gmail.com>
2015-04-22 11:34:30 -0300

    todo comentado, a excepcion de eval, que falta terminar de comentarla. tambien faltaria solucionar ese temilla en quitar de la funcion del modelo

--- src/Lomoba.hs
+++ src/Lomoba.hs
@@ -6,6 +6,9 @@ import Tipos
 -- ---------------------------------Sección 6--------- Lomoba ---------------------------
 
 -- Ejercicio 10
+--foldExp toma una funcion por constructor del tipo Exp y recursiona
+--sobre una instancia de Exp aplicando la funcion correspondiente
+--sobre cada capa del mismo.
 foldExp :: (Prop -> a) -> (a -> a) -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> (a -> a) -> Exp -> a
 foldExp fVarP fNot fOr fAnd fD fB e = 
   let rec = foldExp fVarP fNot fOr fAnd fD fB 
@@ -17,15 +20,26 @@ foldExp fVarP fNot fOr fAnd fD fB e =
                 B e1 -> fB (rec e1) 
      
 -- Ejercicio 11
+--visibilidad toma una instancia de tipo Exp y determina que tan 
+--lejos deberiamos movernos en un grafo de Mundos para determinar la 
+--validez de esa expresion. Utiliza foldExp para recursionar sobre 
+--el parametro, de manera tal que frente a constructores de Diamond 
+--y Box sume una unidad a la visibilidad, en Not no se modifique, 
+--Var sea el caso base (const 0) y para Or/And tome el maximo de 
+--visibilidad entre las dos expresiones involucradas.
 visibilidad :: Exp -> Integer
 visibilidad = foldExp (const 0) id max max (+1) (+1)
 
 -- Ejercicio 12
+--extraer toma una instancia de tipo Exp y devuelve una lista de todas las Props involucradas.
+--Utiliza un foldExp para recursionar sobre el parametro, de manera tal que por cada capa Var genere una lista con esa Prop y luego vaya concatenando las listas resultantes en la recursion sacando repetidos mediante unionS(in)R(epetidos)
 extraer :: Exp -> [Prop]
 extraer = foldExp (:[]) id unionSR unionSR id id
   where unionSR xs ys = xs ++ [y | y <- ys, not (y `elem` xs)]
 
 -- Ejercicio 13
+--eval determina si la Expresion e es valida en el mundo de acuerdo al modelo pasado por parametro.
+-- 
 eval :: Modelo -> Mundo -> Exp -> Bool
 eval modelo mundo e = eval' modelo e mundo
 
@@ -39,13 +53,21 @@ eval' (K g fv) = foldExp fVarP fNot fOr fAnd fD fB
         fB = (\rec -> \mundo -> and [rec m | m <- (vecinos g mundo)])
         
 -- Ejercicio 14
+--valeEn retorna todos los mundos del grafo con el cual esta organizado el modelo en los cuales vale la expresion parametro.
+--utiliza eval para determinar durante la recursion de foldr si vale la expresion en cada mundo y de ser asi lo incluye en la lista.
 valeEn :: Exp -> Modelo -> [Mundo]
 valeEn e model@(K g r) = foldr (\mundo rec -> if (eval model mundo e) then (mundo:rec) else rec) [] (nodos g)
 
 -- Ejercicio 15
+--quitar elimina todos los mundos del modelo en los cuales no vale la expresion parametro, devolviendo un nuevo Modelo donde su grafo no incluye dichos mundos.
+--Recursiona sobre la lista 'precalculada' de mundos en los que no vale la expresion y los elimina del grafo utilizando sacarNodo. De esta manera no modificamos el grafo antes de determinar efectivamente cuales mundos no validan la expresion.
 quitar :: Exp -> Modelo -> Modelo
 quitar e model@(K g r) = (K (foldr (\nodo rec -> sacarNodo nodo rec ) g (valeEn (Not e) model)) r)
 
+--quitar2 e model@(K g r) = (K (foldr (\nodo rec -> sacarNodo nodo rec ) g (valeEn (Not e) model)) (\x -> if ))
+
 -- Ejercicio 16
+--cierto determina si la expresion e es valida en el modelo, es decir, si vale para todos los mundos del mismo.
+--evalua la expresion en cada mundo formando una cadena de ANDs de forma tal que si en algun mundo la expresion no es valida, el resultado sea false.
 cierto :: Modelo -> Exp -> Bool
 cierto model@(K g r) e = foldr (\mundo rec -> (eval model mundo e) && rec) True (nodos g)
