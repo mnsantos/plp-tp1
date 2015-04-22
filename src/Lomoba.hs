17967d9a226b2c04a2ed22e7651e22c55184f700
martin.santos <martin.santos@despegar.com>
2015-04-22 02:03:43 -0300

    eval agregado. fatlaria testearlo

--- src/Lomoba.hs
+++ src/Lomoba.hs
@@ -6,34 +6,37 @@ import Tipos
 -- ---------------------------------Sección 6--------- Lomoba ---------------------------
 
 -- Ejercicio 10
-foldExp :: (Prop -> a) -> (a -> a) -> (a -> a -> a) -> (a -> a -> a) -> (Exp -> a -> a) -> (Exp -> a -> a) -> Exp -> a
+foldExp :: (Prop -> a) -> (a -> a) -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> (a -> a) -> Exp -> a
 foldExp fVarP fNot fOr fAnd fD fB e = 
   let rec = foldExp fVarP fNot fOr fAnd fD fB 
   in case e of  Var p -> fVarP p
                 Not e1 -> fNot (rec e1)
                 Or e1 e2 -> fOr (rec e1) (rec e2)
                 And e1 e2 -> fAnd (rec e1) (rec e2)
-                D e1 -> fD e1 (rec e1) 
-                B e1 -> fB e1 (rec e1) 
+                D e1 -> fD (rec e1) 
+                B e1 -> fB (rec e1) 
      
 -- Ejercicio 11
 visibilidad :: Exp -> Integer
-visibilidad = foldExp (const 0) id max max (const (+1)) (const (+1))
+visibilidad = foldExp (const 0) id max max (+1) (+1)
 
 -- Ejercicio 12
 extraer :: Exp -> [Prop]
-extraer = foldExp (:[]) id unionSR unionSR (const id) (const id)
+extraer = foldExp (:[]) id unionSR unionSR id id
   where unionSR xs ys = xs ++ [y | y <- ys, not (y `elem` xs)]
 
 -- Ejercicio 13
 eval :: Modelo -> Mundo -> Exp -> Bool
-eval (K g fv) m = foldExp fVarP fNot fOr fAnd fD fB
-  where fVarP p = m `elem`(fv p)
-        fNot e1 = not (e1)
-        fOr e1 e2 = e1 || e2
-        fAnd e1 e2 = e1 && e2
-        fD e1 e1r = or [(eval (K g fv) m' e1) | m' <- (vecinos g m)]
-        fB e1 e1r = and [(eval (K g fv) m' e1) | m' <- (vecinos g m)]
+eval modelo mundo e = eval' modelo e mundo
+
+eval' :: Modelo -> Exp -> Mundo -> Bool
+eval' (K g fv) = foldExp fVarP fNot fOr fAnd fD fB
+  where fVarP = (\p -> \mundo -> mundo `elem` (fv p))
+        fNot = (\rec -> \mundo -> not (rec mundo))
+        fOr = (\rec1 rec2 -> \mundo -> (rec1 mundo) || (rec2 mundo))
+        fAnd = (\rec1 rec2 -> \mundo -> (rec1 mundo) && (rec2 mundo))
+        fD = (\rec -> \mundo -> or [rec m | m <- (vecinos g mundo)])
+        fB = (\rec -> \mundo -> and [rec m | m <- (vecinos g mundo)])
         
 -- Ejercicio 14
 valeEn :: Exp -> Modelo -> [Mundo]
@@ -42,8 +45,6 @@ valeEn e model@(K g r) = foldr (\mundo rec -> if (eval model mundo e) then (mund
 -- Ejercicio 15
 quitar :: Exp -> Modelo -> Modelo
 quitar e model@(K g r) = (K (foldr (\nodo rec -> sacarNodo nodo rec ) g (valeEn (Not e) model)) r)
-quitar2 e model@(K g r) = foldr (\x rec -> if (eval model x e) then rec else (K (sacarNodo x g) r)) model (nodos g)
--- quitar2 funciona mal, quitarla.
 
 -- Ejercicio 16
 cierto :: Modelo -> Exp -> Bool
