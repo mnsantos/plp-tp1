5e0710eeb30a8e94ddc9028763fa1ef71bcbbc44
agajda <agajda@gmail.com>
2015-04-23 00:43:16 -0300

    Retoque de comentarios y ejemplos 2

--- src/Lomoba.hs
+++ src/Lomoba.hs
@@ -4,11 +4,13 @@ import Tipos
 
 
 -- ---------------------------------Sección 6--------- Lomoba ---------------------------
-
+        
+        
 -- Ejercicio 10
---foldExp toma una funcion por constructor del tipo Exp y recursiona
---sobre una instancia de Exp aplicando la funcion correspondiente
---sobre cada capa del mismo.
+-- -------------------------------------------------------------------------- --
+-- Por cada constructor de Exp aplica una funcion especifica al resultado     --
+-- recursivo de la expresion sin el constructor.                              --
+-- -------------------------------------------------------------------------- --
 foldExp :: (Prop -> a) -> (a -> a) -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> (a -> a) -> Exp -> a
 foldExp fVarP fNot fOr fAnd fD fB e = 
   let rec = foldExp fVarP fNot fOr fAnd fD fB 
@@ -20,26 +22,42 @@ foldExp fVarP fNot fOr fAnd fD fB e =
                 B e1 -> fB (rec e1) 
      
 -- Ejercicio 11
---visibilidad toma una instancia de tipo Exp y determina que tan 
---lejos deberiamos movernos en un grafo de Mundos para determinar la 
---validez de esa expresion. Utiliza foldExp para recursionar sobre 
---el parametro, de manera tal que frente a constructores de Diamond 
---y Box sume una unidad a la visibilidad, en Not no se modifique, 
---Var sea el caso base (const 0) y para Or/And tome el maximo de 
---visibilidad entre las dos expresiones involucradas.
+-- -------------------------------------------------------------------------- --
+-- Utiliza foldExp para hacer recursion sobre la expresion de forma tal que   --
+-- frente a constructores de Diamond y Box sume una unidad a la visibilidad,  --
+-- Var no aporte, en Not no se modifique, y que Or/And tome el maximo entre   --
+-- las dos expresiones de ambos operadores involucrados.                      --
+-- Ejemplo1: visibilidad (parse "p") = 0                                      --
+-- Ejemplo2: visibilidad (parse "<>!<>p") = 2                                 --
+-- Ejemplo3: visibilidad (parse "[](<>p && <>[]q)") = 3                       --
+-- -------------------------------------------------------------------------- --
 visibilidad :: Exp -> Integer
 visibilidad = foldExp (const 0) id max max (+1) (+1)
 
 -- Ejercicio 12
---extraer toma una instancia de tipo Exp y devuelve una lista de todas las Props involucradas.
---Utiliza un foldExp para recursionar sobre el parametro, de manera tal que por cada capa Var genere una lista con esa Prop y luego vaya concatenando las listas resultantes en la recursion sacando repetidos mediante unionS(in)R(epetidos)
+-- -------------------------------------------------------------------------- --
+-- Utiliza foldExp para hacer recursion sobre la expresion de forma tal que   --
+-- por cada constructor Var genere una lista con esa Prop y este es el unico  --
+-- caso en que se agregan variables. Los constructores Not, Diamond y Box no  --
+-- cambian las variables por lo cual se opera con la identidad. En el caso de --
+-- Or/And podrian agregar repeticiones por lo cual se unen las variables      --
+-- usando unionSR que utiliza listas por compresion para evitar esto.         -- 
+-- Ejercicio1: extraer (parse "[](<>p && <>[]q)") = ["p","q"]                 --
+-- Ejercicio1: extraer (parse "[](<>p && <>[]p)") = ["p"]                     --
+-- -------------------------------------------------------------------------- --
 extraer :: Exp -> [Prop]
 extraer = foldExp (:[]) id unionSR unionSR id id
   where unionSR xs ys = xs ++ [y | y <- ys, not (y `elem` xs)]
 
 -- Ejercicio 13
---eval determina si la Expresion e es valida en el mundo de acuerdo al modelo pasado por parametro.
--- 
+-- -------------------------------------------------------------------------- --
+-- Explicacion bla bla bla porque eval' bla bla bla                           --
+--                                                                            --
+-- Ejemplos: Sea "ejKripke" un modelo que representa la figura 1 del TP       -- 
+-- Ejemplo1: eval ejKripke 1 (parse "p && []q") = True                        --
+-- Ejemplo2: eval ejKripke 1 (parse "p && <>r") = True                        --
+-- Ejemplo3: eval ejKripke 1 (parse "[]r") = False                            --
+-- -------------------------------------------------------------------------- --
 eval :: Modelo -> Mundo -> Exp -> Bool
 eval modelo mundo e = eval' modelo e mundo
 
@@ -53,21 +71,44 @@ eval' (K g fv) = foldExp fVarP fNot fOr fAnd fD fB
         fB = (\rec -> \mundo -> and [rec m | m <- (vecinos g mundo)])
         
 -- Ejercicio 14
---valeEn retorna todos los mundos del grafo con el cual esta organizado el modelo en los cuales vale la expresion parametro.
---utiliza eval para determinar durante la recursion de foldr si vale la expresion en cada mundo y de ser asi lo incluye en la lista.
+-- -------------------------------------------------------------------------- --
+-- Se utiliza eval para determinar durante la recursion de foldr si vale la   --
+-- expresion en cada mundo del modelo y de ser asi lo incluye en la lista.    --
+-- Ejemplos: Sea "ejKripke" un modelo que representa la figura 1 del TP       --
+-- Ejemplo1: valeEn (parse "p && []q") ejKripke = [1]                         --
+-- Ejemplo2: valeEn (parse "[]r") ejKripke = [2,3]                            --
+-- -------------------------------------------------------------------------- --
 valeEn :: Exp -> Modelo -> [Mundo]
 valeEn e model@(K g r) = foldr (\mundo rec -> if (eval model mundo e) then (mundo:rec) else rec) [] (nodos g)
 
--- Ejercicio 15
---quitar elimina todos los mundos del modelo en los cuales no vale la expresion parametro, devolviendo un nuevo Modelo donde su grafo no incluye dichos mundos.
---Recursiona sobre la lista 'precalculada' de mundos en los que no vale la expresion y los elimina del grafo utilizando sacarNodo. De esta manera no modificamos el grafo antes de determinar efectivamente cuales mundos no validan la expresion.
+-- Ejercicio 15  
+-- -------------------------------------------------------------------------- --
+-- Se utiliza valeEn para calcular la lista de mundos en que no vale la       --
+-- expresion, se eliminan estos mundos del grafo del modelo usando foldr con  --
+-- sacarNodo y finalmente se devuelve un modelo con este nuevo grafo y la     --
+-- misma relacion.                                                            --
+-- De esta manera no modificamos el grafo antes de determinar efectivamente   --
+-- cuales mundos no validan la expresion.                                     --
+-- Ejemplos: Sea "ejKripke" un modelo que representa la figura 1 del TP y     --
+--           "showGrafo" un proyector del grafo de un modelo                  --
+-- Ejemplo1: showGrafo (quitar (parse "p && []q") ejKripke) =                 --
+--[                                                                           --
+-- 1 -> []                                                                    --
+--]                                                                           --
+-- -------------------------------------------------------------------------- --
 quitar :: Exp -> Modelo -> Modelo
 quitar e model@(K g r) = (K (foldr (\nodo rec -> sacarNodo nodo rec ) g (valeEn (Not e) model)) r)
 
---quitar2 e model@(K g r) = (K (foldr (\nodo rec -> sacarNodo nodo rec ) g (valeEn (Not e) model)) (\x -> if ))
-
 -- Ejercicio 16
---cierto determina si la expresion e es valida en el modelo, es decir, si vale para todos los mundos del mismo.
---evalua la expresion en cada mundo formando una cadena de ANDs de forma tal que si en algun mundo la expresion no es valida, el resultado sea false.
+-- -------------------------------------------------------------------------- --
+-- Se utiliza foldr y eval para evaluar la expresion en cada mundo del modelo --
+-- y aplicando And a esta cadena se verifica si la expresion es valida en     --
+-- todos ellos.                                                               --  
+-- Ejemplos: Sea "ejKripke" un modelo que representa la figura 1 del TP       --
+-- Ejemplo1:                                                                  --
+--   cierto ejKripke (parse "p && []q") = False                               --
+-- Ejemplo2:                                                                  --
+--   cierto (quitar (parse "p && []q") ejKripke) (parse "p && []q") = True    --
+-- -------------------------------------------------------------------------- --
 cierto :: Modelo -> Exp -> Bool
 cierto model@(K g r) e = foldr (\mundo rec -> (eval model mundo e) && rec) True (nodos g)
